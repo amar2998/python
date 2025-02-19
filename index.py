0e3e879eb54d2039ad3d77678a8677a47c006ff6
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-19 12:53:21 +0000

    first commit

---
+++ index.py
@@ -0,0 +1,29 @@
+# a=89
+# b=87
+# sum=a+b
+
+# sum=int("45")
+# b=87
+
+# print(sum+b)
+
+# ab="amar"
+# print(int(ab))# it cause error
+
+# bcd=45654
+# print(str(bcd))
+# print(type(bcd))
+# data=int(input("enter your name"));
+# print(data)
+# print(type(data))
+
+# data='y'
+# print(type(data))
+
+# if(23>21):
+#     print("true")
+# else:
+#     print("false")
+
+# str="amarsonunath"
+# print(str[:-1])//amarsonunat 
