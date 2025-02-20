6557060a71d0d79f585092cd6a7ce1064f80dc41
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-20 15:10:21 +0000

    first

---
+++ string.py
@@ -0,0 +1,33 @@
+string = "amar sonu nath"
+# print(string[0])
+# # string[0]='u'#immutable
+
+# #method
+# print(string.capitalize())
+# print(string[4:]) #slice
+
+# print(string.replace("nath","math"))
+
+# print(string.upper())
+# print(string.lower())
+# str="    stringthis is    "
+# print(len(str));
+# str=str.strip() # remove whitespace from front and back
+# print(len(str));
+# str2=string+str
+# print(str2)
+# print(str.join(string))
+# fstring
+age=34
+name=f"my name is amar and i am {age} years old "
+print(name)
+print(string.count("a"))
+print(string.startswith('a'))
+print(string.endswith('a'))
+print(string.encode(encoding="ascii",errors="backslashreplace"))
+print(string.swapcase())
+print(string.index('a'))
+print(string.find("amar"))
+print(string.split(" "))
+
+
