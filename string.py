dd0113d54f1df04b3629a15f8f4d9b3a69772349
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-20 15:29:37 +0000

    second

--- string.py
+++ string.py
@@ -1,22 +1,22 @@
 string = "amar sonu nath"
-# print(string[0])
-# # string[0]='u'#immutable
+print(string[0])
+# string[0]='u'#immutable
 
-# #method
-# print(string.capitalize())
-# print(string[4:]) #slice
+#method
+print(string.capitalize())
+print(string[4:]) #slice
 
-# print(string.replace("nath","math"))
+print(string.replace("nath","math"))
 
-# print(string.upper())
-# print(string.lower())
-# str="    stringthis is    "
-# print(len(str));
-# str=str.strip() # remove whitespace from front and back
-# print(len(str));
-# str2=string+str
-# print(str2)
-# print(str.join(string))
+print(string.upper())
+print(string.lower())
+str="    stringthis is    "
+print(len(str));
+str=str.strip() # remove whitespace from front and back
+print(len(str));
+str2=string+str
+print(str2)
+print(str.join(string))
 # fstring
 age=34
 name=f"my name is amar and i am {age} years old "
