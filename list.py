65ba35a0d52decb66e64717b8ca14f96d4740b8d
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-21 03:27:28 +0000

    fistlist

---
+++ list.py
@@ -0,0 +1,28 @@
+list=[1,2,3,4,5,6]
+print(list[5]) # access
+list[2]=33
+print(list) # mutable
+
+list.append(23)
+print(list)
+list.insert(2,4)
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+
+print(list)
+list.reverse()
+print(list)
+list.pop()
+print(list)
+
+print(list.index(2))
+list2=list.copy()
+print(list2)
+list2.remove(5)
+print(list2)
+print(list2.count(4))
+print(list.count(4))
+list2.clear()
+print(list2)
