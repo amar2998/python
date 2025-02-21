ee67332f6e3c743021d68680b63298493a0c7583
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-21 04:09:04 +0000

    first

---
+++ tuple.py
@@ -0,0 +1,10 @@
+tupple=(1,2,3,4,5,6,7)
+print(tupple)
+# tupple[0]=45#immutable
+# tupple.push(23)
+# print(tupple)
+# tupple=(1,2,3,4,) this is also valid 
+tup2=(5,6,7,8,9,0)
+print(tupple+tup2)# join
+print(tup2.count(2))
+print(tupple.index(7))
